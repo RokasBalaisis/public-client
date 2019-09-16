a3d5f9c3f22122221c8d42c22958b4fe9a65d512
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-16 10:32:48 +0300

    authservice fix#2

--- services/AuthHttpInterceptor.js
+++ services/AuthHttpInterceptor.js
@@ -28,7 +28,7 @@ app.factory('AuthHttpInterceptor', ['$q', '$rootScope', '$localStorage', '$injec
                 // server, if we're within the ttl_refresh period.
                 var deferred = $q.defer();
                 // We inject $http, otherwise we will get a circular ref
-                $injector.get('$http', AuthService).post(API + '/reissue', {}, {
+                $injector.get('$http', 'AuthService').post(API + '/reissue', {}, {
                     headers: {
                         Authorization: AuthService.getToken()
                     }
