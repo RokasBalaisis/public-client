30a3f4fd4b121c2df408b083714829e61d986dfd
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-21 22:21:13 +0300

    auth interceptor adjustments

--- services/AuthHttpInterceptor.js
+++ services/AuthHttpInterceptor.js
@@ -13,7 +13,6 @@ app.factory('AuthHttpInterceptor', ['$q', '$rootScope', '$localStorage', '$injec
             // If there is a token in the headers, retrieve it
             if (new_token = response.headers("Authorization")) {
                 // Store the token in storage
-                console.log("new token added!");
                 $rootScope.storeAuthToken(new_token);
             }
             // Return the reponse to the app
@@ -23,47 +22,11 @@ app.factory('AuthHttpInterceptor', ['$q', '$rootScope', '$localStorage', '$injec
         responseError: function(rejection) {
             // If the error is 401 related
             if (rejection.status === 401) {
-                // We're going to get attempt to refresh the token on the
-                // server, if we're within the ttl_refresh period.
-                var deferred = $q.defer();
-                // We inject $http, otherwise we will get a circular ref
-                $injector.get('$http').post(API + '/reissue', {}, {
-                    headers: {
-                        Authorization: $rootScope.getAuthToken()
-                    }
-                }).then(function(response) {
-
-                    // If this request was successful, we will have a new
-                    // token, so let's put it in storage
-                    $rootScope.storeAuthToken(response.token);
-                    // Now let's send the original request again
-                    $injector.get('$http')(response.config)
-                        .then(function(response) {
-
-                            // The repeated request was successful! So let's put
-                            // this response back to the original workflow
-                            return deferred.resolve(response);
-                        }, function() {
-                            // Something went wrong with this request
-                            // So we reject the response and carry on with 401
-                            // error  
-                            $rootScope.$broadcast('auth - logout');
-                            return deferred.reject();
-                        })
-                }, function() {
-                    // Refreshing the token failed, so let's carry on with
-                    // 401
-                    $rootScope.$broadcast('auth - logout');
-                    return deferred.reject();
-                });
-                // Now we continue with the 401 error if we've reached this
-                // point
-                return deferred.promise;
+                return $rootScope.$broadcast('auth - logout');
             }
             if (rejection.status === 500) {
                 console.log("it's a 500!");
             }
-            return $q.reject(rejection);
         }
     };
 }]);
