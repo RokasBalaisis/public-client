69c68c6f62c376c5d2da8f607a0897ba25933004
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-23 10:04:46 +0300

    bugfix

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -9,11 +9,26 @@ app.controller('MainController', ['$scope', 'ApiService', 'AuthService', '$rootS
             password: $scope.password
         }
         var promise = AuthService.login($data);
-    }
+    };
+
+    $scope.register = function() {
+        var $data = {
+            username: $scope.username,
+            registration_email: $scope.registration_email,
+            registration_password: $scope.registration_password
+        }
+        var promise = ApiService.register($data);
+
+        promise.then(function(response) {
+            console.log(promise);
+        }, function(error) {
+            console.log(error);
+        })
+    };
 
     $scope.logout = function() {
         var promise = AuthService.logout();
-    }
+    };
 
     $scope.redirectHome = function() {
         $location.path('');
--- services/ApiService.js
+++ services/ApiService.js
@@ -3,6 +3,9 @@ app.factory("ApiService", function($http, API) {
         login: function($data) {
             return $http.post(API + '/login', JSON.stringify($data));
         },
+        register: function($data) {
+            return $http.post(API + '/register', JSON.stringify($data));
+        },
         users_index: function() {
             return $http.get(API + '/users');
         }
