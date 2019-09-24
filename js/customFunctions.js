cbadfd1b1035db9c7cb47ab539cb667b49009cd5
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-24 22:04:44 +0300

    visual update

--- js/customFunctions.js
+++ js/customFunctions.js
@@ -1,11 +1,14 @@
 document.addEventListener('click', function(event) {
-    if (!document.getElementById('popupWindow').contains(event.target)) {
-        if (document.getElementById('open-register').contains(event.target)) {
-            $(".overlay").fadeToggle("slow", "linear");
-            $("#popupWindow").fadeToggle("slow", "linear");
-        } else if ($(document.getElementById('popupWindow')).is(":visible")) {
-            $(".overlay").fadeToggle("slow", "linear");
-            $("#popupWindow").fadeToggle("slow", "linear");
+    if (document.body.contains(document.getElementById('popupWindow'))) {
+        if (!document.getElementById('popupWindow').contains(event.target)) {
+            if (document.getElementById('open-register').contains(event.target)) {
+                $(".overlay").fadeToggle("slow", "linear");
+                $("#popupWindow").fadeToggle("slow", "linear");
+            } else if ($(document.getElementById('popupWindow')).is(":visible")) {
+                $(".overlay").fadeToggle("slow", "linear");
+                $("#popupWindow").fadeToggle("slow", "linear");
+            }
         }
     }
+
 });
