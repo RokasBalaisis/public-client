3797365a3bcf1278cbe5cf467fc7a5bf27811c31
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-25 22:14:10 +0300

    registration fully working

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -2,7 +2,6 @@ app.controller('MainController', ['$scope', 'ApiService', 'AuthService', '$rootS
     $scope.name = 'Angular ';
     $scope.hasRegisterFormErrors = false;
 
-
     $scope.login = function() {
         $rootScope.currentPage = "login";
         var $data = {
@@ -21,18 +20,44 @@ app.controller('MainController', ['$scope', 'ApiService', 'AuthService', '$rootS
         var promise = ApiService.register($data);
 
         promise.then(function(response) {
-            if (response.status == 401 || response.status == 422) {
+            if (response.status == 401 || response.status == 422 || response.status == 409) {
                 $scope.hasRegisterFormErrors = true;
+
                 var regMssgArray = [];
                 angular.forEach(response.data, function(value, key) {
                     this.push(value[0]);
                 }, regMssgArray);
                 $scope.registrationErrorMessages = regMssgArray;
+
+            } else if (response.status == 201) {
+                $scope.successfulRegistrationEvent(response);
             }
         }, function(error) {
 
         })
     };
+
+    $scope.closeRegisterForm = function() {
+        $(".overlay").fadeToggle("slow", "linear");
+        if ($(document.getElementById('popupWindow')).is(":visible")) {
+            $scope.hasRegisterFormErrors = false;
+            $("#popupWindow").hide();
+        } else {
+            $("#popupWindow").show();
+            $scope.username = "";
+            $scope.registration_email = "";
+            $scope.registration_password = "";
+        }
+
+
+    }
+
+    $scope.successfulRegistrationEvent = function(response) {
+        $scope.successMessage = response.data['message'];
+        $scope.closeRegisterForm();
+        $('#successful-alert').fadeToggle("slow", "linear");
+        $('#successful-alert').delay(1000).fadeToggle("slow", "linear");
+    }
 
     $scope.logout = function() {
         var promise = AuthService.logout();
--- js/customFunctions.js
+++ js/customFunctions.js
@@ -2,14 +2,23 @@ document.addEventListener('click', function(event) {
     if (document.body.contains(document.getElementById('popupWindow'))) {
         if (!document.getElementById('popupWindow').contains(event.target)) {
             if (document.getElementById('open-register').contains(event.target)) {
-                $(".overlay").fadeToggle("slow", "linear");
-                $("#popupWindow").fadeToggle("slow", "linear");
+                eventFire(document.getElementById('custom-register-close'), 'click');
             } else if ($(document.getElementById('popupWindow')).is(":visible")) {
-                $(".overlay").fadeToggle("slow", "linear");
-                $("#popupWindow").hide();
-                $(".alert.alert-danger").delay(500).hide();
+                eventFire(document.getElementById('custom-register-close'), 'click');
             }
         }
     }
 
 });
+
+
+
+function eventFire(el, etype) {
+    if (el.fireEvent) {
+        el.fireEvent('on' + etype);
+    } else {
+        var evObj = document.createEvent('Events');
+        evObj.initEvent(etype, true, false);
+        el.dispatchEvent(evObj);
+    }
+}
